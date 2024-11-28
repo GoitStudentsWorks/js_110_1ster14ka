b3727d85fdee979ab6f370a346fdaec2ab4a2f55
1ster14ka <danyazybastik@gmail.com>
2024-11-28 13:41:25 +0200

    fixed modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal-overlay');
 list.addEventListener('click', event => {
   if (event.target.classList.contains('modal-link')) {
     modal.classList.remove('active');
+    document.body.style.overflow = 'auto';
   } else {
     return;
   }
